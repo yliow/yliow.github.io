ab73cbdee47eb773c214bbaa1c1b20ca935487e5
Yihsiang Liow <yliow@ccis.edu>
2023-04-08 17:06:13 -0400

    no msg

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ def toc():
     return r'''
     <h2><a id="top">TOC</a></h2>
     <ul>
+      <li><a href="#software">Software</a></li>      
       <li><a href="#courses">Courses</a></li>      
       <li><a href="#tutorials">Tutorials</a></li>
       <li><a href="#wanna-feed-your-brain">Wanna feed your brain?</a></li>      
