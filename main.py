cea90a6a1e9b77b12dcd6beabf1a1ce0e00192cc
Yihsiang Liow <yliow@ccis.edu>
2023-04-08 23:35:59 -0400

    no msg

--- main.py
+++ main.py
@@ -370,11 +370,12 @@ def others():
 
 def pics():
     return r'''
-        <a href="index.html">Home</a> | <a href="index.html">Outreach</a>
 
 <html>  
   %(head)s
   <body style="font-family: arial">
+    <a href="index.html">Home</a> | <a href="index.html">Outreach</a>
+    
     %(header)s
 
     <h2>Welcome to PiCS</h2>
