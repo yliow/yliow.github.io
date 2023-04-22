3730ca76346fec6628f51871fb54b8a6f8455421
Yihsiang Liow <yliow@ccis.edu>
2023-04-22 14:46:33 -0400

    no msg

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def title(name):
     elif name == 'urics':
         return '''<div>
         URiCS <br>
-        <div style="foreground-color:rgba(0,100,100,100);">Undergraduate Research in Computer Science</div>
+        <div style="color:rgba(0,100,100,100);">Undergraduate Research in Computer Science</div>
         </div>'''
     else:
         return '[NO TITLE]'
