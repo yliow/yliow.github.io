fad823004c04d9583a55c9df6f2217465271a10f
Yihsiang Liow <yliow@ccis.edu>
2023-05-23 16:23:23 -0400

    no msg

---
+++ html_fragments.py
@@ -0,0 +1,26 @@
+#===========================================================================
+# SMALLEST HTML FRAGMENTS
+#===========================================================================
+def cctt():
+    return r'''<a href="http://cctt0.pythonanywhere.com/">Royal Ping Pong Club</a>'''
+def work_study():
+    return r'''<a href="https://docs.google.com/document/d/1nYQtW3TTPIAYHcOeU-KSe2aEOiWHZgn6LA9aybH8pw0/edit?usp=sharing">Work study</a>'''
+def cs101():
+    return r'''<a href='https://docs.google.com/presentation/d/1v_YISntWpguP-t-SwrhjT1537UwmgybZe5mGzwP_11w/edit?usp=sharing'>CS101 Guide to CCCS or ... HHGTT CCCS</a>'''
+
+def publication_about_cccs():
+    return r'''<a href='https://docs.google.com/document/d/1KZ7bve4oEmIHiP8ghwZ2Ky42KGQuxpvJetpYVImTDMY/edit?usp=sharing'>Publications about CCCS</li>'''
+
+def cs_hangout():
+    return r'''<a href ='http://bit.ly/cc_cs_hangout'>CS Hangout</a>.
+    <ul>
+    <li>MF: 4:30PM-6:30PM in BUH103
+    <li>Wed: 4:30-6:30 in COUGAR CAFE
+    <li>TuTh: 3:00PM–5:00PM in BUH102 
+    </ul>
+    '''
+def ciss145():
+    return r'<a href="http://ciss145.pythonanywhere.com">CISS145 (Python)</a>'
+
+def research():
+    return r'''<a href='urics.html'>Research</a>'''
