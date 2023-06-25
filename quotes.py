6d9a21977d211c66ce5c9c99d67440bf30d68500
Yihsiang Liow <yliow@ccis.edu>
2023-06-25 18:24:38 -0400

    no msg

--- quotes.py
+++ quotes.py
@@ -1,4 +1,28 @@
+# Add quotes below. Separate by blank line.
+quotes = '''
+“What matters isn’t extraordinary ability but extraordinary effort.”
+
+“Sometimes it is the people no one can imagine anything of who do the things no one can imagine.”
+
+“It is not that I'm so smart. But I stay with the questions much longer.”
+
+“If you can't explain it simply, you don't understand it well enough.”
+
+“What the imagination seizes as beauty must be truth.”
+
+“Life stands before me like an eternal spring with new and brilliant clothes.”
+
+“He who loves practice without theory is like the sailor who boards ship without a rudder and compass and never knows where he may cast.”
+'''
+
+quotes = quotes.split('\n')
+quotes = [x.strip() for x in quotes if x.strip() != '']
+
 def index():
+    quotes_html = ''
+    for q in quotes:
+        quotes_html = quotes_html + ('<p>%s</p><br>' % q)
+
     return r'''
 
     <!-- pictures -->
@@ -19,46 +43,13 @@ def index():
     <table style="border-collapse:collapse; border : 1px solid white;">
     
     <tr>
-    <td>
-<p>
-“What matters isn’t extraordinary ability but extraordinary effort.”
-</p>
- <br>
-
-<p>
-“Sometimes it is the people no one can imagine anything of who do the things no one can imagine.”
-</p>
- <br>
-
-<p>  
-“It is not that I'm so smart. But I stay with the questions much longer.”
-</p>
- <br>
-
-<p>  
-“If you can't explain it simply, you don't understand it well enough.”
-</p>
- <br>
-
-  <p>  
-“What the imagination seizes as beauty must be truth.”
-</p>
- <br>
-
-<p>
-“Life stands before me like an eternal spring with new and brilliant clothes.”
-</p>
- <br>
-
-<p>
-“He who loves practice without theory is like the sailor who boards ship without a rudder and compass and never knows where he may cast.”
-</p>
-    </td>
-    <td width="10%%">
+    <td>''' + \
+        quotes_html + \
+    '''<td width="10%%">
     <div class="detailImgWrapper"><img src="images/image1.png" width="200"/></div>
     <div class="detailImgWrapper"><img src="images/image3.png" width="200"/></div>
     <div class="detailImgWrapper"><img src="images/image5.png" width="200"/></div>
     </td>
     </tr>
     </table>
-    '''
+    ''' 
