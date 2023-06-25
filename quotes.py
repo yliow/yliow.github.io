b9e421eb259753ef40c1d6db1d0694e6b184c755
Yihsiang Liow <yliow@ccis.edu>
2023-06-25 15:28:14 -0400

    no msg

--- quotes.py
+++ quotes.py
@@ -1,18 +1,32 @@
 def index():
     return r'''
 <h2>Quotes</h2>
-    
+
+<p>
 “What matters isn’t extraordinary ability but extraordinary effort.”
+</p>
 
+<p>
 “Sometimes it is the people no one can imagine anything of who do the things no one can imagine.”
+</p>
 
+<p>  
 “It is not that I'm so smart. But I stay with the questions much longer.”
+</p>
 
+<p>  
 “If you can't explain it simply, you don't understand it well enough.”
+</p>
 
+  <p>  
 “What the imagination seizes as beauty must be truth.”
+</p>
 
+<p>
 “Life stands before me like an eternal spring with new and brilliant clothes.”
+</p>
 
+<p>
 “He who loves practice without theory is like the sailor who boards ship without a rudder and compass and never knows where he may cast.”
+</p>
     '''
