17329f1ba88664ee3345313d445b43782566e01b
Yihsiang Liow <yliow@ccis.edu>
2023-06-25 17:47:08 -0400

    no msg

--- quotes.py
+++ quotes.py
@@ -16,7 +16,7 @@ def index():
 
 <h2>Quotes</h2>
 
-    <table style="border-collapse:collapse; border : 1px solid orange;">
+    <table style="border-collapse:collapse; border : 1px solid white;">
     
     <tr>
     <td>
