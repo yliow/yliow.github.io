6dd302635ade7029db814ab346facac3c323f5c2
Yihsiang Liow <yliow@ccis.edu>
2023-06-25 18:15:03 -0400

    no msg

--- quotes.py
+++ quotes.py
@@ -55,8 +55,8 @@ def index():
 </p>
     </td>
     <td width="10%%">
-    <div class="detailImgWrapper"><img src="images/image1.png"/></div>
-    <div class="detailImgWrapper"><img src="images/image3.png"/></div>
+    <div class="detailImgWrapper"><img src="images/image1.png" width="50"/></div>
+    <div class="detailImgWrapper"><img src="images/image3.png" /></div>
     <div class="detailImgWrapper"><img src="images/image5.png"/></div>
     </td>
     </tr>
