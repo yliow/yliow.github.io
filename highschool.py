68526b7d590d9b83f22bd3657494a3c9a169e082
Yihsiang Liow <yliow@ccis.edu>
2023-07-11 14:38:48 -0400

    no msg

--- highschool.py
+++ highschool.py
@@ -6,9 +6,9 @@ def index():
     <li><a href='https://docs.google.com/document/d/1P-wEe4xqaP7SUwY0QXE8hd2Q0ti_vJb6E5xHRrWZfys/edit'>Programming Challenge meetings</a>
 <li>High school class visits
 <li><a href='https://docs.google.com/document/d/1kBkt_scGsrFe2PGLsrcsf16JyjtKmtYJTobsh0ulGC0'>HS CS Internship</a>
-<li>HS Programming Contests
-<li>Weekend seminars
-<li>Summer camps
-<li>CCPC (Columbia College Programming Club) (2006-2009) 
+<li><a href='https://docs.google.com/document/d/1gjkRumx_jq9VGyxdjBZU9IICerqZr_8wKHaq7nls64Q/'>HS Programming Contests</a>
+<li><a href='https://docs.google.com/document/d/1YeQtYU5EHo241kLfX1dNgJISgWkSCOVmC2GXah7S3ms'>Weekend seminars</a>
+<li><a href='http://bit.ly/pics-summer'>Summer camps</a>
+<li><a href='https://docs.google.com/document/d/1YpeaskuYvo1gjNcXl-o-T50B796XItVobFvR7SeUgOE/edit?usp=sharing'>CCPC (Columbia College Programming Club) (2006-2009)</a> 
 </ul>
     '''
