ee1c4ccb3fdda4f1157760e461353204467ee8a8
Yihsiang Liow <yliow@ccis.edu>
2023-07-11 14:30:01 -0400

    no msg

--- highschool.py
+++ highschool.py
@@ -1,2 +1,14 @@
 def index():
-    return "HIGH SCHOOL"
+    return r'''
+
+<h2>Programs</h2>
+<ul>
+    <li>Programming Challenge meetings
+<li>High school class visits
+<li>HS CS Internship
+<li>HS Programming Contests
+<li>Weekend seminars
+<li>Summer camps
+<li>CCPC (Columbia College Programming Club) (2006-2009) 
+</ul>
+    '''
