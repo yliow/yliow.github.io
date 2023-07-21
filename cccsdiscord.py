3def23bb1e63e97fd0ac21d6244df982bdd25e08
Yihsiang Liow <yliow@ccis.edu>
2023-07-21 18:59:07 -0400

    no msg

--- cccsdiscord.py
+++ cccsdiscord.py
@@ -187,8 +187,9 @@ Michael Fisher
 
     
 <div style="border-style: solid; padding:10px">
-Braydon Hampton blhampton1@cougars.ccis.edu Mar 13, 2020, 2:49:33 PM<br>
-    <img src="images/braydon.jpg" style="width: 250px; margin: 1px 5px; border: #111111;float:right;"/>
+ <img src="images/braydon.jpg" style="width: 250px; margin: 1px 5px; border: #111111;float:right;"/>
+    Braydon Hampton blhampton1@cougars.ccis.edu Mar 13, 2020, 2:49:33 PM<br>
+   
 to cc_cs@googlegroups.com<br>
 Subject: Hangout Hours<br><br>
 
