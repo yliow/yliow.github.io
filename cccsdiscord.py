7c88cbfea55dc0b665e834de33e167f1b0f56f5f
Yihsiang Liow <yliow@ccis.edu>
2023-07-21 18:28:42 -0400

    no msg

--- cccsdiscord.py
+++ cccsdiscord.py
@@ -32,47 +32,10 @@ Here are some snippets:
     tutoring in Math and Computer Science ...
     We will be setting up a discord server ... that will allow us to remain in contact and provide an open forum for students ...&#8221;
     </ul>
-The following was added to my homepage:<br><br>
-
-<div style="border-style: solid; padding:10px">
-    <h2>CCCS Discord</h2>
-
-We are continuing with CS hangout on our CCCS discord server
-<a href="https://discord.gg/UDCX6wF">https://discord.gg/UDCX6wF</a>.
-Same time, different place.
-<b>Thanks go to Brandon, Caleb, Michael for creating/managing it.</b>
-Thanks also to those who made suggestions.
-<ul>
-<li><b>WHAT:</b> (Virtual) CS Hangout and other things
-<li><b>WHY:</b> Get help with studies (CS and math) or just chat.
-<li><b>WHO:</b> CS people
-<li><b>WHERE:</b> CCCS Discord. You can click on
-    <a href="https://discord.gg/UDCX6wF">https://discord.gg/UDCX6wF</a>
-    and go to the server using your browser, but it’s better if
-    you download the app.
-    If you have problems with any of the following steps,
-    send a message to
-    <a href="mailto:email@example.com">cc_cs@googlegroups.com</a>
-    for help – don’t be shy.
-    <ul>
-    <li>Download the discord app:
-    <a href="https://discordapp.com/download">https://discordapp.com/download</a>. 
-    <li>Run the app and go to
-    <a href="https://discord.gg/UDCX6wF">https://discord.gg/UDCX6wF</a>. 
-    <li>Once you get into the CCCS discord server, go to “welcome-and-rules”.
-    <li>After that just click around to enter different rooms/channels.
-    <li>Feel free to ask questions in the rooms/channels. You can also right-click on people’s names (see right panel) especially “WORK STUDY” or “DR.LIOW” to send a direct/private message.
-    </ul>
-<li><b>WHEN:</b> Same as before (see below).
-    <ul>
-    <li>MTuWThF: 6:30-8:30PM (Spring 2021)
-    </ul>
-</ul>
-</div>
 
 <p>
 The following from <a href="https://groups.google.com/g/cc_cs/">cc_cs@googlegroups.com</a>
-    from 2020/03/13 - 2020/03/19 are the messages
+    are the messages (dated 2020/03/13 - 2020/03/19) 
 leading up to the creation of CCCS discord:
 </p>
 
@@ -240,4 +203,43 @@ For the rest of the semester the work study students will be deciding what to do
 Thanks,<br>
 Braydon Hampton<br>
 </div>
+
+    The following was added to my homepage:<br><br>
+
+<div style="border-style: solid; padding:10px">
+    <h2>CCCS Discord</h2>
+
+We are continuing with CS hangout on our CCCS discord server
+<a href="https://discord.gg/UDCX6wF">https://discord.gg/UDCX6wF</a>.
+Same time, different place.
+<b>Thanks go to Brandon, Caleb, Michael for creating/managing it.</b>
+Thanks also to those who made suggestions.
+<ul>
+<li><b>WHAT:</b> (Virtual) CS Hangout and other things
+<li><b>WHY:</b> Get help with studies (CS and math) or just chat.
+<li><b>WHO:</b> CS people
+<li><b>WHERE:</b> CCCS Discord. You can click on
+    <a href="https://discord.gg/UDCX6wF">https://discord.gg/UDCX6wF</a>
+    and go to the server using your browser, but it’s better if
+    you download the app.
+    If you have problems with any of the following steps,
+    send a message to
+    <a href="mailto:email@example.com">cc_cs@googlegroups.com</a>
+    for help – don’t be shy.
+    <ul>
+    <li>Download the discord app:
+    <a href="https://discordapp.com/download">https://discordapp.com/download</a>. 
+    <li>Run the app and go to
+    <a href="https://discord.gg/UDCX6wF">https://discord.gg/UDCX6wF</a>. 
+    <li>Once you get into the CCCS discord server, go to “welcome-and-rules”.
+    <li>After that just click around to enter different rooms/channels.
+    <li>Feel free to ask questions in the rooms/channels. You can also right-click on people’s names (see right panel) especially “WORK STUDY” or “DR.LIOW” to send a direct/private message.
+    </ul>
+<li><b>WHEN:</b> Same as before (see below).
+    <ul>
+    <li>MTuWThF: 6:30-8:30PM (Spring 2021)
+    </ul>
+</ul>
+</div>
+
     '''
