a6e6ac92570ff17934b4870f5d0b3ffbdae52a51
Yihsiang Liow <yliow@ccis.edu>
2024-01-09 12:43:27 -0500

    no msg

--- html_fragments.py
+++ html_fragments.py
@@ -37,11 +37,11 @@ def classes_for_current_semester():
 My classes:
 <ul>
  <li> %s
- <li>ciss380 MWF 10:10AM-11:05AM BUH103
+ <li>ciss445 MWF 09:05AM-11:05AM BUH102
  <li>ciss350 MWF 11:15AM-12:10PM BUH103
+ <li>ciss358 MWF 12:20PM-01:15PM BUH 103
  <li>ciss240 MWF 01:25PM-02:20PM and Tue 02:00PM-02:55PM BUH104
- <li>ciss245 MWF 02:30PM-03:25PM and Thu 02:00PM-02:55PM BUH107
- <li>ciss360 MWF 03:35PM-04:30PM BUH104
+ <li>ciss245 MWF 02:30PM-03:25PM and Thu 02:00PM-02:55PM BUH104
 </ul>
 ''' % alex()
 
