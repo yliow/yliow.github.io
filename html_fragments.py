85dbf76d26e862edea68518e2d4ee4de028c07b6
Yihsiang Liow <yliow@ccis.edu>
2024-02-29 20:49:50 -0500

    no msg

--- html_fragments.py
+++ html_fragments.py
@@ -19,8 +19,8 @@ def cs_hangout():
     return r'''<a href ='http://bit.ly/cc_cs_hangout'>CS Hangout</a>. BRN = Brown Hall. 
     <ul>
     <li><font color='red'>For Fri 3/1, hangout is moved to BUH102 because of a weekend event in Brown Hall.</font>
-    <li>MWF 4:30PM-6:30PM, BRN100 <font color='red'>[NEW]</font>
-    <li>TuTh 3PM-5PM, BRN100 <font color='red'>[NEW]</font>
+    <li>MWF 4:30PM-6:30PM, BRN100 
+    <li>TuTh 3PM-5PM, BRN100 
     </ul>
     '''
 def ciss145():
