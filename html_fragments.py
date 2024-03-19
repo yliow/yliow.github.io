a51f74b508f935166c049e7fc00c6fe174951d87
Yihsiang Liow <yliow@ccis.edu>
2024-03-19 14:20:15 -0400

    no msg

--- html_fragments.py
+++ html_fragments.py
@@ -18,8 +18,7 @@ def publication_about_cccs():
 def cs_hangout():
     return r'''<a href ='http://bit.ly/cc_cs_hangout'>CS Hangout</a>. BRN = Brown Hall. 
     <ul>
-    <li><font color='red'>For Fri 3/1, hangout is moved to BUH102 because of a weekend event in Brown Hall.</font>
-    <li><font color='red'>For Tue 3/19, I'll be at hangout at 4:30PM.</font>
+    <li><font color='red'>For Tue 3/19, I'm visiting an Intro C++ class at CACC and be back to CS hangout at 4:30PM.</font>
     <li>MWF 4:30PM-6:30PM, BRN100 
     <li>TuTh 3PM-5PM, BRN100 
     </ul>
