e97b9d0cab62173d6463c8a639b6ad825a0953f2
Yihsiang Liow <yliow@ccis.edu>
2025-02-12 01:18:12 -0500

    no msg

--- update_date.py
+++ update_date.py
@@ -1,12 +1,12 @@
 import re
 import datetime; NOW = datetime.datetime.now()
 from pyutil import *
-f = readfile('programming-contest.html')
-lines = f.read()
-lines = lines.split('\n')
+s = readfile('programming-contest/index.html')
+lines = s.split('\n')
 t = []
 for line in lines:
-    if t.startswith('Last update:')
-        t = 'Last update: %s' % NOW
+    if line.startswith('Last update:'):
+        line = 'Last update: %s' % NOW
     t.append(line)
-f.write('\n'.join(t))
+s = '\n'.join(t)
+writefile('programming-contest/index.html', s)
