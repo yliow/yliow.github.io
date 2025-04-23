336d1ea69a70b5c38a29bed8e4dd9c4cf35e5480
Yihsiang Liow <yliow@ccis.edu>
2025-04-22 22:50:35 -0400

    no msg

--- html_fragments.py
+++ html_fragments.py
@@ -24,7 +24,7 @@ def reu():
     return r'''
     REU (Research Experiences for Undergraduates - a nationwide NSF funded summer search program)
     <ul>
-    <li>Andrew Woods. <a href=''>REU ?</a>
+    <li>Andrew Woods. <a href=''>REU ?</a> at University of Missouri at Columbia.
     <li>Michael Fisher. <a href='https://reu.rnet.missouri.edu/REU20.html'>REU 2020</a> at University of Missouri at Columbia.
     <!-- "Attack-Defense and Performance Adaptations for Social Virtual Reality Learning Environments".
     (Advisors: Dr. Prasad Calyam / Dr. Khaza Hoque; 
