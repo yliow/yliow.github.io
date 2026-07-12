5b852b49942ccd1b5c9c4ecf10c3dd66c1fd104a
Yihsiang Liow <yliow@ccis.edu>
2026-07-12 14:45:37 -0700

    no msg

--- main.py
+++ main.py
@@ -419,7 +419,7 @@ def software():
     return r'''<h2><a id="software">Software</a> <a href="#top" style="font-size:16px">top</a></h2>
 <ul>
     <li>Software downloads for classes:
-    <span style='color:red'>NEW***</span> <a href="https://docs.google.com/document/d/1fstvjlxEb45D3R6THZuLOnTZ3C-JSWWN8pdwW5KMVm8/">README</a>
+     <a href="https://docs.google.com/document/d/1fstvjlxEb45D3R6THZuLOnTZ3C-JSWWN8pdwW5KMVm8/">README</a>
     | <span style='color:red'>NEW***</span> <a href='https://drive.google.com/drive/folders/1FlF_EY1n5t8UbcXkdtACZxCq5OGjVgbj?usp=sharing'>Software folder</a></li>
     <li>Link for SDL <span style='color:red'>NEW*** </span><a href="https://drive.google.com/file/d/1qjM_Aayja4dOFHYdT5Bnwkl_ZKp-QeaM/view?usp=sharing">demo.tar</a>
     (for CISS245 game development)</li>
